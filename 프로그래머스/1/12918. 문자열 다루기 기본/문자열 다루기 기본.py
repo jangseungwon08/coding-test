a74af1c8e559b7167f34761919c42d2bfc36f27e
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-02 22:32:18 +0900

    Update 문자열 다루기 기본.py

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,5 +1,8 @@
 def solution(s):
     answer = True
+    #(s의길이가 4또는 6일때) 그리고 s가 숫자로만 이루어져있는지 알 수 있는
+    #str메서드에서 사용가능한 isdigit함수를 사용해서 4또는 6을 만족하고 숫자로만 이루어져 있으면
+    #true반환
     if (len(s) == 4 or len(s) == 6) and s.isdigit():
         answer = True
     else:
