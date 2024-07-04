fde822fcc04bfac7a684389a9bf3b80080cd9e21
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-04 15:29:55 +0900

    [level 1] Title: 내적, Time: 0.18 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.py
+++ 프로그래머스/1/70128. 내적/내적.py
@@ -2,7 +2,7 @@ def solution(a, b):
     answer = 0
     #a의 길이와 b의 길이가 같으니까 a의 길이를 구한 후 a의 각 원소값과 b의 각 원소값을 곱해서
     #다 더해주면 내적 길이가 나온다.
-    #sum을 이용해서도 구할 수 있다.
+    #sum과 zip을 이용해서도 구할 수 있다.
     for i in range(len(a)):
         answer += a[i] * b[i]
     return answer
