9c4034192f48be69e16ee36af8695da5eaaf55b8
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-16 21:54:19 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 9.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -2,9 +2,10 @@ def solution(t, p):
     answer = 0
     #임시 리스트 생성
     temp = []
-    #예시 1번기준) 0부터 3까지의 범위를 한다.
+    # len(t)-len(p) +1을 해줌으로써 부분문자열의 인덱스 개수를 셀 수 있다.
+    #따라서 7-3+1=5이다 0부터 4까지의 인덱스 -> 5개의 부분문자열 추출 가능
     for i in range(len(t)-len(p) +1):
-        temp.append(t[i:i+len(p)]) #인덱스 i번째부터 i+len(p)-1번째까지
+        temp.append(t[i:i+len(p)]) #인덱스 i번째부터 i+len(p)-1번째까지 문자 추출
         
     for j in temp:
         #정수 값 비교를 위해서 int로 형 변환 후 비교해준다
