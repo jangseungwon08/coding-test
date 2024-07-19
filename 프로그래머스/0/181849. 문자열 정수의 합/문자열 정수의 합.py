1fd73b06faa3590169d325659f0640121f6d0050
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-19 17:04:41 +0900

    Update 문자열 정수의 합.py

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -1,6 +1,6 @@
 def solution(num_str):
     answer = 0
-    #map함수를 이용하여 문자열 형식의 num_str을 int형식으로 바꿔준다. 그 후 list형변환을 하여 
-    #이터러블한 객체로 만들어서 sum함수를 이용하여 합을 구한다.
+    #map함수를 이용하여 문자열 형식의 num_str을 int형식으로 바꿔준다.
+    #string은 이터러블한 객체이기때문에 sum함수를 이용하여 합을 구한다.
     answer = sum(map(int,num_str))
     return answer
