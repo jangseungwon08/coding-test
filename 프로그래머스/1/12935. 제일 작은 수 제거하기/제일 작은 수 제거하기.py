3c634cbbae984c40aeb1bac403df23b1ee6c5b26
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-19 23:51:53 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.19 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,8 +1,11 @@
 def solution(arr):
     answer = []
-    #가장 작은수를 제거하기 위해서 sort함수와 reverse = True를 사용하여                            arr을 내림차순으로 정렬한다.
-    #arr.sort(reverse = True)
-    #그리고 pop함수를 사용하여 가장 마지막 원소를 리턴하고 제거한다.
+    '''
+    가장 작은수를 제거하기 위해서 sort함수와 reverse = True를 사용하여                            arr을 내림차순으로 정렬한다.
+    arr.sort(reverse = True)
+    그리고 pop함수를 사용하여 가장 마지막 원소를 리턴하고 제거한다.
+    '''
+    #위 방식으로 하면 안되고, 리스트가 원래의 순서를 유지해야하기 때문에 sort를 사용하면 안된다.
     arr.remove(min(arr))
     #arr배열에 원소값이 존재할 경우
     if arr:
