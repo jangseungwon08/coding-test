72d1ec736db14905ea908df6c6fe78955622b890
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-20 19:52:48 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,5 +1,6 @@
 def solution(my_string, is_suffix):
     answer = 0
+    '''
     temp = []
     #접두사인지 확인하기와 같은 문제
     for i in range(len(my_string)):
@@ -8,4 +9,7 @@ def solution(my_string, is_suffix):
             answer = 1
         else:
             answer = 0
+            '''
+    #len(is_suffix)를 사용하여 길이만큼 뒤에서부터 끝까지 인덱싱을 한 값이 is_suffix와 값이 같      다.
+    answer = int(my_string[-len(is_suffix):] == is_suffix)
     return answer
