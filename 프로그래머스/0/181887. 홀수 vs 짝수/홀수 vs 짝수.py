dd74d1286cdee358f484027fc0af3210fe95af26
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-22 21:19:55 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -3,10 +3,12 @@ def solution(num_list):
     odd = 0
     even = 0
     for i in range(len(num_list)):
-        if i %2:
-            odd += num_list[i]
-        else:
+        #i %2 가 값이 존재한다. -> 인덱스에서 홀수이다. 하지만 가장 첫번째 원소를 1번 원소라고했           으니까 even(짝수)에 대입시켜야된다.
+        if i % 2:
             even += num_list[i]
+            #반대로 생각하면 된다.
+        else:
+            odd += num_list[i]
     if odd > even:
         answer = odd
     else:
