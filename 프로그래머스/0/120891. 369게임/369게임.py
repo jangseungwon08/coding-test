e8a13cef359721b87492077ff523ac2b2d44b1cd
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-22 22:46:56 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,8 +1,8 @@
 def solution(order):
     answer = 0
-    #각 숫자가 있는 것을 확인하기 위해서 list(str())형식으로 29423처럼 여러 숫자가 있는 것도 
-    # 하나하나 셀 수 있게 하기 위해 list(str()) 형식으로 변환하여 for문을 돌린다.
-    for i in list(str(order)):
+    #각 숫자가 있는 것을 확인하기 위해서 str()형식으로 29423처럼 여러 숫자가 있는 것도 
+    # 하나하나 셀 수 있게 하기 위해 str() 형식으로 변환하여 for문을 돌린다.
+    for i in str(order):
         #"3"이 i안에 있으면 answer1 증가 
         if "3" in i:
             answer += 1
