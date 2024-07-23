ac7c85bef5662b34b6ff2d341f839e8ae8fc35b3
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-23 23:04:10 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -2,10 +2,10 @@ def solution(a, b, c):
     answer = 0
     #a와 b와 c의 값이 같을 때
     if a ==b==c:
-        answer = (a+b+c) * ((a*a) + (b*b) + (c*c)) * ((a*a*a) + (b*b*b) + (c*c*c))
+        answer = (a+b+c) * ((a**2) + (b**2) + (c**2)) * ((a**3) + (b**3) + (c**3))
     #a와 b 또는 a와 c 또는 b와 c가 같을 때
     elif a == b or a==c or b==c:
-        answer = (a+b+c) * ((a*a) + (b*b) + (c*c))
+        answer = (a+b+c) * ((a**2) + (b**2) + (c**2))
         #위의 두개의 조건문을 만족하지 못할 때
     else:
         answer = a+b+c
