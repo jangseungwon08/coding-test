bd2e941f702bbf2f5db820867888950c06effc8e
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-25 17:28:27 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.48 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,4 +1,13 @@
 def solution(n):
-    answer = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+    #list 컴프리헨션
+    #answer = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+    #answer에 [0]*n을 0 1 2 행까지 구한다
+    answer = [[0]*n for _ in range(n)]
     
+    for i in range(n):
+        for j in range(n):
+            #i의 값과 j의 값이 같으면
+            if i == j:
+                #answer[i][j]번째에 1을 저장
+                answer[i][j] = 1
     return answer
