b50efc6b8080dfb35f1088a6c109ea2345e23981
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-26 21:43:17 +0900

    [level 0] Title: 세로 읽기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -0,0 +1,12 @@
+def solution(my_string, m, c):
+    answer = ''
+    #일단 my_string문자열을 각 원소로 나누어줘야되니까
+    temp = []
+    #my_string for문 돌리면서
+    for i in my_string:
+        #temp배열에 하나씩 다 넣어준다
+        temp.append(i)
+        #temp배열 슬라이싱으로 c-1번째부터 m칸 만큼 step밟아주면
+    temp = temp[c-1::m]
+    answer = ''.join(temp)
+    return answer
