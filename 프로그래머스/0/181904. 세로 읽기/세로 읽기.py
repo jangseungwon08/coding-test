1d8ef933f95c05a6eb98f420f6995a37a1c90fd4
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-26 21:44:45 +0900

    Update 세로 읽기.py

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,4 +1,5 @@
 def solution(my_string, m, c):
+    '''
     answer = ''
     #일단 my_string문자열을 각 원소로 나누어줘야되니까
     temp = []
@@ -9,4 +10,6 @@ def solution(my_string, m, c):
         #temp배열 슬라이싱으로 c-1번째부터 m칸 만큼 step밟아주면
     temp = temp[c-1::m]
     answer = ''.join(temp)
+    '''
+    answer = my_string[c-1::m]
     return answer
