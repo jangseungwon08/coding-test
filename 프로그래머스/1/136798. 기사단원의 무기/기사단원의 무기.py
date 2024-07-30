13ee3dd7a971279f8806346bdd8455ae42bffba7
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-30 17:33:29 +0900

    [level 1] Title: 기사단원의 무기, Time: 932.90 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -4,12 +4,13 @@ def solution(number, limit, power):
     for i in range(1,number+1):
         #temp_count 0 으로 초기화
         temp_count = 0
+        #제곱근 위 아래의 약수 개수가 같다는 공식을 이용(i의 제곱근이 정수 라는 제한)
         #1부터 i의 제곱근 +1까지 ex i가 4이면 1부터 2까지의 범위까지 
         for j in range(1,int(i**(1/2)) + 1):
             # i에 j를 나눈 나머지 값이 없으면 약수
             if i % j == 0:
                 temp_count += 1
-                #i에 j를 정수형 나누기 한 값이 j의 값과 같지 않으면 약수
+                #i의 약수 개수 중복 약수를 취급하기 위해서 만약에 i가 36 j가 6이면 36//6 = j                    이므로 중복 약수이다.
                 if i // j != j:
                     temp_count += 1
         #각 number의 약수를 구하고 temp_count가 limit보다 크면 answer에 power저장
