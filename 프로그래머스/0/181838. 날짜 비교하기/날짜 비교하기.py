3a21d9b6c47a490cfcc6ac138850502f97cb060c
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-30 17:43:42 +0900

    Update 날짜 비교하기.py

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,5 +1,7 @@
 def solution(date1, date2):
+    #date1이 앞서는 날짜니까 28일이 29일보다 앞선다. 따라서 date1의 전체 slicing결과가              date2의 슬라이싱 결과보다 작으면 1을 리턴
     if date1[::] < date2[::]:
         return 1
+    #date1의 슬라이싱 결과가 크거나 같으면 0리턴
     else:
         return 0
