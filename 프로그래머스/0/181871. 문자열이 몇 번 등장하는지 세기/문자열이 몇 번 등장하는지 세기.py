e75a86afd337f0aba7b842d8d51902cc9af6b060
jangseungwon08 <jangseungwon9819@gmail.com>
2024-07-30 18:20:57 +0900

    Update 문자열이 몇 번 등장하는지 세기.py

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -1,6 +1,11 @@
 def solution(myString, pat):
     answer = 0
+    #myString을 인덱스 형식으로 순회
     for i in range(len(myString)):
+        #myString[i:len(pat)+i:] 의 값이 pat과 같으면
+        # i = 0 1 2 3 4 5
+        #myString[0:2:] myString[1:3] myString[2:4] myString[3:5]형식으로 접근
         if myString[i:len(pat)+i:] == pat:
+            #answer에 1증가
             answer += 1
     return answer
