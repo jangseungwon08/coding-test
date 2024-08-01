7ac5d46c0bbf9b9f04fff2dd1b6158c17da32633
jangseungwon08 <jangseungwon9819@gmail.com>
2024-08-01 23:20:45 +0900

    [level 0] Title: A로 B 만들기, Time: 0.33 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,16 @@
+def solution(before, after):
+    answer = 0
+    temp_before = 0
+    temp_after = 0
+    #after와 before길이 같으니까 인덱스 형식으로 순회
+    for i in range(len(before)):
+        #temp_before에 before의 아스키 코드 값 누적
+        temp_before += ord(before[i])
+        #temp_after에 after의 아스키 코드 값 누적
+        temp_after += ord(after[i])
+        #누적한 값이 같으면 1 다르면 0
+    if temp_before == temp_after:
+        answer = 1
+    else:
+        answer = 0
+    return answer
