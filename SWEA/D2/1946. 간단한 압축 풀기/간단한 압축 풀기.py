73cf24dca0ee7ff0abb766e1afd29019a31356f5
jangseungwon08 <jangseungwon9819@gmail.com>
2024-11-05 01:52:08 +0900

    [D2] Title: 간단한 압축 풀기, Time: 101 ms, Memory: 45,052 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
@@ -1,18 +1,13 @@
 T = int(input())
-for i in range(1,T+1):
+for i in range(1, T+1):
+    ori_str = ""
+    ans_list = []
     N = int(input())
-    #문자열을 다 넣기위한 all문자열 
-    all = ''
+    for j in range(N):
+        alpha, num = input().split()
+        ori_str += alpha*int(num)
+    for k in range(0, len(ori_str), 10):
+        ans_list.append(ori_str[k:k+10])
     print('#'+str(i))
-    #N만큼 순회하면서
-    for _ in range(N):
-        #ci값과 ki값을 입력받음
-        ci , ki = input().split()
-        #all에 ci에 int(ki)를 곱한값을 all에 누적저장
-        all += ci*int(ki)
-        #0부터len(all)까지 10step씩 순회하면서
-    for j in range(0,len(all), 10):
-        #all의 j부터 j+10까지 출력
-        print(all[j:j+10])
-        #j가 0 이면 0부터 9인덱스까지 원소를 출력
-        #그 다음step은 10인덱스부터 19까지 원소출력
+    for l in ans_list:
+        print(l)
