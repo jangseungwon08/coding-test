2d40d42d1243b05fc8ad7cbf242de76cbc2e1f69
jangseungwon08 <jangseungwon9819@gmail.com>
2024-11-12 23:27:56 +0900

    [D3] Title: 회문의 회문, Time: 106 ms, Memory: 45,312 KB -BaekjoonHub

--- SWEA/D3/20019. 회문의 회문/회문의 회문.py
+++ SWEA/D3/20019. 회문의 회문/회문의 회문.py
@@ -1,18 +1,20 @@
 T = int(input())
-for i in range(1, T+1):
-    t_str = input()
-    s_t_str = t_str[:(len(t_str)-1)//2]
-    e_t_str = t_str[len(t_str) - ((len(t_str)-1)//2):]
-    reverse_str = ''.join(reversed(t_str))
-    reverse_s_t_str = ''.join(reversed(s_t_str))
-    reverse_e_t_str = ''.join(reversed(e_t_str))
-    if t_str == reverse_str:
-        if s_t_str == reverse_s_t_str:
-            if e_t_str == reverse_e_t_str:
-                print('#'+str(i), "YES")
+for tc in range(1, T+1):
+    s = input()
+    half_length = (len(s)-1) //2
+    #reversed함수를 사용한 뒤에 값을 띄우려면 ''.join함수 사용해야됨
+    reversed_s = ''.join(reversed(s))
+    start_s = s[:half_length]
+    reversed_start = ''.join(reversed(start_s))
+    end_s = s[half_length+1:]
+    reversed_end = ''.join(reversed(end_s))
+    if s == reversed_s:
+        if start_s == reversed_start:
+            if end_s == reversed_end:
+                print('#'+str(tc), "YES")
             else:
-                print('#'+str(i), "NO")
+                print('#'+str(tc), "NO")
         else:
-            print('#' + str(i), "NO")
+            print('#'+str(tc), "NO")
     else:
-        print('#' + str(i), "NO")
+        print('#'+str(tc), "NO")
