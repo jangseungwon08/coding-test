5769aac3a538899292550fe08e572d4aeef64cf2
jangseungwon08 <jangseungwon9819@gmail.com>
2024-11-16 22:02:36 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 118 ms, Memory: 46,608 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -12,10 +12,17 @@ for tc in range(1,11):
             if s == reverse_s:
                 count += 1
     #세로
+    #row값 고정
     for i in range(8):
+        #col값 j 0부터 8-N까지
         for j in range(9-N):
+            #j 부터 j+N까지 순회하면서 k번째 행의 값, i번째 열의 값을 join
+            #문 활용하여 s에 저장
             s = ''.join(N_list[k][i] for k in range(j, j+N))
+            #reverse_s 는 s의 역순
             reverse_s = s[::-1]
+            #s가 reverse_s와 같으면
             if s == reverse_s:
+                #count값 증가
                 count += 1
     print('#'+str(tc), count)
