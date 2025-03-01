413855fa5ba5e1121cb288e6d0f187f486478aee
jangseungwon08 <jangseungwon9819@gmail.com>
2025-03-01 13:19:32 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 120 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*
         int[] chess = {1, 1, 2, 2, 2, 8};
         int[] ans = new int[6];
         String[] str = br.readLine().split(" ");
@@ -17,5 +18,27 @@ public class Main {
         for(int i = 0; i< ans.length;i++){
             System.out.print(ans[i] + " ");
         }
+         */
+        int king = 1;
+        int queen = 1;
+        int look = 2;
+        int bishop = 2;
+        int knight = 2;
+        int pawn = 8;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        king = king - Integer.parseInt(st.nextToken());
+        queen = queen - Integer.parseInt(st.nextToken());
+        look = look - Integer.parseInt(st.nextToken());
+        bishop = bishop - Integer.parseInt(st.nextToken());
+        knight = knight - Integer.parseInt(st.nextToken());
+        pawn = pawn - Integer.parseInt(st.nextToken());
+
+        System.out.print(king + " ");
+        System.out.print(queen + " ");
+        System.out.print(look + " ");
+        System.out.print(bishop + " ");
+        System.out.print(knight + " ");
+        System.out.print(pawn + " ");
     }
     }
