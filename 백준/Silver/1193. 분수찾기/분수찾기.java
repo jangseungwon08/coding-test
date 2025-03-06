d18e084b70b214f80730aa5fb453531c13f91125
jangseungwon08 <jangseungwon9819@gmail.com>
2025-03-06 19:06:15 +0900

    [Silver V] Title: 분수찾기, Time: 124 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -6,7 +6,7 @@ public class Main {
         int x = Integer.parseInt(br.readLine());
         int cross_count = 1;//대각선 칸 개수 : 1개부터 시작함
         int prev_cross_count = 0;//이전 대각선 칸의 개수 누적 합
-
+        br.close();
         while (true) {
             // x가 전 대각선 칸의 누적 합 + 해당 대각선의 칸의 개수보다 작거나 같다는 뜻은 해당 대각선 칸에 x번째 분수가 존재한다는 것
             if (x <= prev_cross_count + cross_count) {
