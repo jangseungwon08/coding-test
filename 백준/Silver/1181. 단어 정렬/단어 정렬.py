ca346b37a207fb15c7ce062cb17c66d1b01a2a74
jangseungwon08 <jangseungwon9819@gmail.com>
2025-03-25 22:09:01 +0900

    [Silver V] Title: 단어 정렬, Time: 76 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,17 +1,17 @@
-#입력받을 문자열 개수
+import sys
+input = sys.stdin.readline
 N = int(input())
-#입력받을 문자열을 리스트 형식으로 초기화
-N_list = list()
-#N만큼 범위 순회하면서
+N_list = []
 for _ in range(N):
-    #N_list에 input()값을 append해줌
-    N_list.append(input())
-#다 입력받고 중복제거를 위하여 set함수 사용하여 제거 하고 다시 list형식으로 변환
+    # readline으로 입력받으면 개행문자(\n)이 포함되기 때문에 
+    # strip함수를 사용하여 한줄 씩 띄워지는 걸 방지
+    # strip 매개변수가 생략되거나 None이면 char인자의 기본값은 
+    # 공백을 제거하도록 한다.
+    N_list.append(input().strip())
 N_list = list(set(N_list))
-#sorted함수 사용하여 N_list를 오름차순하는데 len(x)를 기준으로 먼저 하고 그
-#뒤에는 x(사전순)를 기준으로 한다.
-#여기서 lambda x는 익명함수를 정의 x는 각 원소를 가리킴
-N_list = sorted(N_list, key = lambda x : (len(x), x))
-#i를 출력
+# sort의 람다함수 x라 칭하고 x길이를 기준으로 오름차순하는데
+# 길이가 같으면 사전순으로 오름차순 정렬
+N_list.sort(key = lambda x:(len(x), x))
+
 for i in N_list:
     print(i)
