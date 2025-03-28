fc5ec78a2660ac6ad515e6e333aaa0c6abb9847d
jangseungwon08 <jangseungwon9819@gmail.com>
2025-03-28 20:17:46 +0900

    [Silver V] Title: 숫자 카드, Time: 588 ms, Memory: 121096 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,21 +1,14 @@
-#상근이가 가지고있는 숫자 카드의 개수
+import sys
+input = sys.stdin.readline
 N = int(input())
-#검색연산이 많아지기 때문에 N_list에 set(집합)을 사용 따라서 O(1)의 시간복잡도가 걸려서
-#시간복잡도 감소
-N_list = set(map(int,input().split()))
-#M개의 숫자
+N_set = set(map(int, input().split()))
 M = int(input())
-#상근이가 가지고있는 숫자인지 아닌지 구분하는 list
-M_list = list(map(int,input().split()))
-#M개의 수에 대해서니까 M_list의 길이 만큼 [0]을 곱해서 ans_list에  저장
-ans_list = []
-#N_list순회하면서
-for i in M_list:
-    #M_list에 i의 원소값이 있으면
-    if i in N_list:
-        ans_list.append(1)
+M_set = list(map(int, input().split()))
+res = []
+for i in M_set:
+    if i in N_set:
+        res.append(1)
     else:
-        ans_list.append(0)
-#순회다하고 ans_list출력
-for i in ans_list:
+        res.append(0)
+for i in res:
     print(i, end = ' ')
