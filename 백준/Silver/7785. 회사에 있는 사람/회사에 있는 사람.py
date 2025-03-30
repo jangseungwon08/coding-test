dd56914e5e53babae96a3c63c86f5134590902d0
jangseungwon08 <jangseungwon9819@gmail.com>
2025-03-30 12:41:35 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 184 ms, Memory: 47968 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,12 +1,17 @@
-n = int(input())
-n_dic = dict()
-ans_list = []
-for _ in range(n):
-    name, enter= input().split()
-    n_dic[name] = enter
-for i in n_dic.keys():
-    if n_dic.get(i) == "enter":
-        ans_list.append(i)
-ans_list = sorted(ans_list, reverse = True)
-for i in ans_list:
-    print(i)
+import sys
+input = sys.stdin.readline
+N = int(input())
+name_enter = dict()
+for _ in range(N):
+    name, enter = input().strip().split()
+    name_enter[name] = enter
+# 딕셔너리를 iterator에서 원소 삭제 시 list로 변경 후 진행
+key_list = list(name_enter)
+for k in key_list:
+    if 'leave'== name_enter[k]:
+        del name_enter[k]
+#sorted사용 시 name_enter의 key값만 빠져나가기 때문에 괜찮음
+res_list = sorted(name_enter, reverse = True)
+
+for k in res_list:
+    print(k)
