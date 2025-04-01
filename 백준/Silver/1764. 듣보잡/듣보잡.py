374c5366e2c6e296137b6f924211cee79ca78930
jangseungwon08 <jangseungwon9819@gmail.com>
2025-04-01 21:26:48 +0900

    [Silver IV] Title: 듣보잡, Time: 72 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,13 +1,17 @@
-n, m = map(int, input().split())
-n_set = set()
-ans_list = []
-for _ in range(n):
-    n_set.add(input())
-for _ in range(m):
-    name = input()
-    if name in n_set:
-        ans_list.append(name)
-ans_list = sorted(ans_list)
-print(len(ans_list))
-for i in ans_list:
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().strip().split())
+N_set = set()
+total_list = []
+#듣지못한사람
+for _ in range(N):
+    N_set.add(input().strip())
+#보도 못한 사람
+for _ in range(M):
+    name = input().strip()
+    if name in N_set:
+        total_list.append(name)
+total_list.sort()
+print(len(total_list))
+for i in total_list:
     print(i)
