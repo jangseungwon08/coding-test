fb5cb92aa7bd8d69c01ecc9c57ee1eb9b3e12366
jangseungwon08 <jangseungwon9819@gmail.com>
2025-04-05 01:38:50 +0900

    Update 괄호.py

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,26 +1,21 @@
-t = int(input())
-for _ in range(t):
-    stack = []
-    s = input()
-    #s문자열 순회하면서
-    for i in s:
-        #i가 '('이면 stack에 (문자 append
+import sys
+input = sys.stdin.readline
+N = int(input().strip())
+for _ in range(N):
+    N_list = []
+    case = input().strip()
+    state = True
+    for i in case:
         if i == '(':
-            stack.append('(')
-            #i 가 )이고 stack이 비어있지않으면 -> '(' 문자가 있다는 뜻이니
-            #stack pop
-        elif i == ')':
-            if stack:
-                stack.pop()
-            #stack이 비어있지 않으면 NO프린트한 뒤 루프 탈출
-            else:
-                print('NO')
-                break
-            #break문을 끊기지 않고 
-        #stack이 비어있으면 이라는 뜻이므로 괄호가 다 알맞게 완성되었다는 뜻
-    else:    
-        if not stack:
-            print('YES')
-        #stack이 있으면 괄호가 완성이 안되었다는 뜻
+            N_list.append(i)
         else:
-            print('NO')
+            if N_list:
+                N_list.pop(-1)
+            else:
+                print("NO")
+                break
+    else:
+        if  not N_list:
+            print("YES")
+        elif N_list:
+            print("NO")
