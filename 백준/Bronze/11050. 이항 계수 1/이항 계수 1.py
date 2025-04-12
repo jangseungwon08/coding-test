4e56d61620e969329bb3c29f4633a1007dc6bfa1
jangseungwon08 <jangseungwon9819@gmail.com>
2025-04-12 12:41:09 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,3 +1,17 @@
-import math
-n, k = map(int, input().split())
-print(math.factorial(n)// (math.factorial(n-k) * math.factorial(k)))
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+# 5 2 -> 5 4 / 2 1  ==> N! / K!
+comb_count = 0
+combN = 1
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+factK = factorial(K)
+while comb_count < K:
+    comb_count += 1
+    combN *= N
+    N -= 1
+print(combN // factK)
