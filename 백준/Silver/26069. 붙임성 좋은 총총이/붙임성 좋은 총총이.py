612b34290c3590d91fbcecc90aa12f098261f40f
jangseungwon08 <jangseungwon9819@gmail.com>
2025-04-14 22:27:13 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -1,9 +1,15 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-N_set = {"ChongChong"}
+N_set = set()
 for _ in range(N):
     a , b =input().split()
+    if a == "ChongChong":
+        N_set.add(a)
+        N_set.add(b)
+    elif b == "ChongChong":
+        N_set.add(b)
+        N_set.add(a)
     if a in N_set or b in N_set:
         N_set.add(a)
         N_set.add(b)
