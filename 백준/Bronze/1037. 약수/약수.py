88bc63f23fb3625f6281a355acd6512ebd3b3c77
jangseungwon08 <jangseungwon9819@gmail.com>
2025-04-14 18:22:58 +0900

    [Bronze I] Title: 약수, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -1,12 +1,8 @@
-#n의 약수의 개수
-n = int(input())
-#n의 진짜 약수
-n_list = list(map(int,input().split()))
-#n의 개수가 1이면 n_list에 제곱을 한 값을 출력
-if len(n_list) == 1:
-    print(n_list[0]**2)
-#양수 A가 N의 진짜 약수가 되려면 N이 A의 배수
-#A가 1과 N이 아니어야 한다 라고 제한사항이 있다.
-#따라서 가장 작은 원소값과 큰 원소값을 곱한값이 A의 값이 된다.
+import sys
+input = sys.stdin.readline
+N = int(input())
+N_list = list(map(int, input().split()))
+if len(N_list) == 1:
+    print(N_list[0]**2)
 else:
-    print(max(n_list)*min(n_list))
+    print(max(N_list) * min(N_list))
