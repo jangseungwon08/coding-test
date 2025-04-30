be3bf35c91e71c3d307a96ba5e1399bb0b838e59
jangseungwon08 <jangseungwon9819@gmail.com>
2025-04-30 23:01:19 +0900

    [level 2] Title: 피보나치 수, Time: 26.48 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -3,5 +3,9 @@ def solution(n):
     memo[0] = 0
     memo[1] = 1
     for i in range(2,n+1):
+        #1234567을 mod하는 이유는 
+        #fib(100)만해도 64비트int의 범위를 넘어서기때문에
+        #1234567을 하여 나머지를 memo에 저장한다. 
+        #단지 값만 보는것이 아닌 overflow를 어떻게 효율적으로 정리하느냐도 보는듯
         memo[i] = (memo[i-1] + memo[i-2]) % 1234567
     return memo[-1]
