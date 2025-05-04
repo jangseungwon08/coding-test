f8b68e8125ef1ccdc473132ea063e8ca3f79757a
jangseungwon08 <jangseungwon9819@gmail.com>
2025-05-04 15:50:49 +0900

    [Gold IV] Title: 문자열 폭발, Time: 880 ms, Memory: 42956 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,12 +1,16 @@
-S = input()
-bomb = input()
+import sys
+
+input = sys.stdin.readline
+
+S = input().strip()
+bomb = input().strip()
 temp = []
 for i in S:
     temp.append(i)
     if bomb == ''.join(temp[len(temp) - len(bomb):len(temp)]):
         for i in range(len(bomb)):
             temp.pop()
-    S = temp
+S = temp
 if not S:
     print("FRULA")
 else:
