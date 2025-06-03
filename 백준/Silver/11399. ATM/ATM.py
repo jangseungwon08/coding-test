a5519631582552adf8eea6e4f16e8e679742936d
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-03 22:30:26 +0900

    [Silver IV] Title: ATM, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,10 +1,10 @@
 N = int(input())
 N_list = list(map(int,input().split()))
-N_list2 = sorted(N_list)
+N_list.sort()
 ans = 0
 for i in range(N):
     temp = 0
     for j in range(i+1):
-        temp += N_list2[j]
+        temp += N_list[j]
     ans += temp
 print(ans)
