06689d0dc3746cbfdfd5fdeaf86be4c569d92a78
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-07 16:58:06 +0900

    [Silver III] Title: queuestack, Time: 200 ms, Memory: 49744 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -0,0 +1,19 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+M = int(input())
+C = list(map(int,input().split()))
+q = deque([])
+
+for i in range(N):
+    # 큐인 자료구조만 모아주기기
+    if A[i] == 0:
+        q.append(B[i])
+
+for i in range(M):
+    q.appendleft(C[i])
+    print(q.pop(), end = ' ')
