a41047e0b91c2ab2daecff10a5c5a43569aab7bb
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-07 17:16:33 +0900

    [Silver III] Title: queuestack, Time: 188 ms, Memory: 49744 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.py
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -14,6 +14,11 @@ for i in range(N):
     if A[i] == 0:
         q.append(B[i])
 
+#오른쪽으로 갈 수록 나중에 들어온 값으로 처리했다.
+# ex) deque([1,4])
+#  --> deque([2,1,4]) -> deque([2,1]) 
+# why? -> stack은 append pop하면 stack의 특성상 LIFO이기때문에 자기자신을 
+# 넣고 빼기 때문에 stack은 상관이 없다
 for i in range(M):
     q.appendleft(C[i])
     print(q.pop(), end = ' ')
