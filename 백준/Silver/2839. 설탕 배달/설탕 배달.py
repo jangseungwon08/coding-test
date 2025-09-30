12e62f739c9d7a1e5b5416e75108b04316de5663
jangseungwon08 <jangseungwon9819@gmail.com>
2025-09-30 13:05:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,19 +1,13 @@
-n = int(input())
-three, five =0, 0
-if n % 5 ==0:
-    print(n // 5)
+N = int(input())
+count = 0
+while N > 0:
+    if N % 5 == 0:
+        count += N // 5
+        N %= 5
+    else:
+        N -= 3
+        count += 1
+if N < 0:
+    print(-1)
 else:
-    while n > 0:
-        n -= 3
-        three +=1
-        if n% 5 == 0:
-            five += n // 5
-            print(three + five)
-            break
-        elif n == 1 or n == 2:#5에서 3을 빼고 난 나머지는 1 or 2이기때문
-            print(-1)
-            break
-        elif n == 0:
-            three = n // 3
-            print(three)
-            break
+    print(count)
