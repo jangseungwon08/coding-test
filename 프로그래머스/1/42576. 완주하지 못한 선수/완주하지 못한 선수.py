1fbe7b983f895f7634fcc617b8b5e439db579a23
jangseungwon08 <jangseungwon9819@gmail.com>
2025-10-02 15:35:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.34 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,4 @@
+import collections
 def solution(participant, completion):
-    answer = ''
-    completion_dict = dict()
-    for i in participant:
-        if i in completion_dict:
-            completion_dict[i] += 1
-        else:
-            completion_dict[i] = 1
-    for j in completion:
-        if j in completion_dict:
-            completion_dict[j] -= 1
-    for k,v in completion_dict.items():
-        if v > 0:
-            answer = k
-            break
-    return answer
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
