1dd280cdd22fc9432ee059366ac8e791cc1bdeb9
jangseungwon08 <jangseungwon9819@gmail.com>
2025-10-02 15:32:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.08 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 34 MB, 시간: 52.87 ms
+메모리: 33.1 MB, 시간: 44.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 28일 17:23:40
+2025년 10월 02일 15:32:42
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,21 +1,16 @@
 def solution(participant, completion):
     answer = ''
-    #참가자이름을 key로 명수를 value로 하기위한 딕셔너리
-    part_dict = {}
-    #participant리스트를 순회
-    for v in participant:
-        #value값을 key값으로 하고 v에 해당하는 값을 가져온다. v가 part_dict에 없으면 기본적으로           0으로 시작하고 1을 더해줌
-        part_dict[v] = part_dict.get(v, 0) + 1
-        #completion순회하면서
-    for i in completion:
-        #part_dict[i]에 1을 빼줌
-        part_dict[i] -= 1
-        #part_dict[i]의 value값이 0이면
-        if part_dict[i] == 0:
-            #part_dict[i]를 제거
-            del part_dict[i]
-    #완주하고 남은 part_dcit의 key값을 for문 순회하면서 
-    for i in part_dict.keys():
-        #answer에 i를 누적저장 후 리턴
-        answer += i
+    completion_dict = dict()
+    for i in participant:
+        if i in completion_dict:
+            completion_dict[i] += 1
+        else:
+            completion_dict[i] = 1
+    for j in completion:
+        if j in completion_dict:
+            completion_dict[j] -= 1
+    for k,v in completion_dict.items():
+        if v > 0:
+            answer = k
+            break
     return answer
