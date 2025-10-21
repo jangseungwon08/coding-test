b84b8069936044a78109236a8e76ee87aad22e71
jang seungwon <jangseungwon9819@gmail.com>
2025-10-21 15:07:34 +0900

    [level 1] Title: 과일 장수, Time: 97.96 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,6 @@
-from collections import deque
 def solution(k, m, score):
     answer = 0
     score = sorted(score, reverse=True)
-    q = deque(score)
-    for i in range(0, len(score), m):
-        if len(score) - i < m:
-            break
-        tmp = [q.popleft() for _ in range(i, i+m)]
-        answer += min(tmp) * len(tmp)
+    for i in range(m-1, len(score), m):
+        answer += score[i] * m
     return answer
