9edfe7adf4c19a591177e8bbd9ac978728bed9b7
jangseungwon <jangseungwon9819@gmail.com>
2025-10-26 13:47:49 +0900

    [level 1] Title: 과일 장수, Time: 109.61 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,6 +1,8 @@
 def solution(k, m, score):
     answer = 0
     score = sorted(score, reverse=True)
-    for i in range(m-1, len(score), m):
-        answer += score[i] * m
+    for i in range(0, len(score), m):
+        if i + m > len(score):
+            break
+        answer += score[i+m-1] * m
     return answer
