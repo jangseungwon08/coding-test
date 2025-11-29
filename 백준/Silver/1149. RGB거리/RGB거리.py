919fff529262c25af4a025fbb2a70bdf64e7c86e
jangseungwon <jangseungwon9819@gmail.com>
2025-11-29 22:41:23 +0900

    [Silver I] Title: RGB거리, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,12 +1,10 @@
-N = int(input())
-N_list = []
-dp = [[0]*3 for _ in range(N)]
-for _ in range(N):
-    N_list.append(list(map(int,input().split())))
-dp[0] = N_list[0]
+n = int(input())
+n_list = []
+for _ in range(n):
+    n_list.append(list(map(int, input().split())))
 
-for i in range(1,N):
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + N_list[i][0]
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + N_list[i][1]
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + N_list[i][2]
-print(min(dp[N-1]))
+for i in range(1,n):
+    n_list[i][0] += min(n_list[i-1][1], n_list[i-1][2])
+    n_list[i][1] += min(n_list[i-1][0], n_list[i-1][2])
+    n_list[i][2] += min(n_list[i-1][1], n_list[i-1][0])
+print(min(n_list[-1]))
