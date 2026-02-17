c082f181cd4f2184d785a6a1392aec3b72a8a41d
jangseungwon <jangseungwon9819@gmail.com>
2026-02-17 16:20:44 +0900

    [Gold IV] Title: 알파벳, Time: 4136 ms, Memory: 19116 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,53 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int r, c, maxCount;
+    static char[][] arr;
+    static List<Character> isVisited;
+    //    상우하좌
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        arr = new char[r][c];
+        isVisited = new ArrayList<>();
+        for (int i = 0; i < r; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+        isVisited.add(arr[0][0]);
+        backTrack(0, 0, 1);
+        System.out.println(maxCount);
+    }
+
+    static void backTrack(int row, int col, int cnt) {
+        if (cnt > maxCount) {
+            maxCount = cnt;
+        }
+        for (int i = 0; i < 4; i++) {
+            int nr = row + dr[i];
+            int nc = col + dc[i];
+            if (nr >= 0 && nr < r && nc >= 0 && nc < c) {
+//                현재까지 지나온 칸의 알파벳과 달라야됨
+                if (!isVisited.contains(arr[nr][nc])) {
+                    isVisited.add(arr[nr][nc]);
+                    backTrack(nr, nc, cnt + 1);
+//                    들어갔다 나온 값 다시 remove해줘야지 다음 탐색 방해 x
+                    isVisited.remove(isVisited.size()-1);
+                }
+            }
+        }
+    }
+}
