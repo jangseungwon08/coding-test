438b86e364c08dc6e0ee9176f3275632c19e068b
jangseungwon <jangseungwon9819@gmail.com>
2026-02-19 23:09:17 +0900

    [Gold IV] Title: 연구소, Time: 992 ms, Memory: 300336 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[][] graph;
+    static int count;
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+    static int n, m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        graph = new int[n][m];
+        count = 0;
+        for(int i = 0; i< n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dfs(0);
+        System.out.println(count);
+    }
+    static void dfs(int cnt){
+        if(cnt == 3){
+//            bfs로직 시작?
+            bfs();
+            return;
+        }
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j< m; j++){
+                if(graph[i][j] == 0){
+                    graph[i][j] = 1;
+                    dfs(cnt+1);
+                    graph[i][j] = 0;
+                }
+            }
+        }
+    }
+    static void bfs(){
+        Queue<int[]> q = new ArrayDeque<>();
+        int cnt = 0;
+        int[][] copyArr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            copyArr[i] = graph[i].clone(); // 행 단위로 clone을 해야 완벽히 복사됨
+        }
+        for(int i = 0; i< n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (copyArr[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                }
+            }
+        }
+            while(!q.isEmpty()) {
+                int[] current = q.poll();
+                int r = current[0];
+                int c = current[1];
+                for (int k = 0; k < 4; k++) {
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                        if (copyArr[nr][nc] == 0) {
+                            copyArr[nr][nc] = 2;
+                            q.offer(new int[]{nr, nc});
+                        }
+                    }
+                }
+            }
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j< m; j++){
+                if(copyArr[i][j] == 0){
+                    cnt++;
+                }
+            }
+        }
+        count = Math.max(cnt , count);
+    }
+}
