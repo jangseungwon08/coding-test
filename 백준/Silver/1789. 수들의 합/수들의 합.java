59ad91cb4824ea2093d6221010d237e7f15c3685
jangseungwon <jangseungwon9819@gmail.com>
2026-02-26 22:37:13 +0900

    [Silver V] Title: 수들의 합, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.java
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,17 +10,16 @@ public class Main {
         List<Integer> memo = new ArrayList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long s = Long.parseLong(br.readLine());
-        int idx = 0;
-        memo.add(1);
-        int num = 1;
-        s -= num;
+        long num = 1;
+        long idx = 0;
         while(s >= 0){
-//            memo.get(idx)에서 num을 더한 값에 s를 뺌
-            int temp = num + memo.get(idx);
-            s -= temp;
-            if(s < 0 ) break;
-            memo.add(memo.get(idx++) + num);
+            s -= num;
+            if(s < 0){
+                break;
+            }
+            num++;
+            idx++;
         }
-        System.out.println(memo.get(memo.size()-1));
+        System.out.println(idx);
     }
 }
