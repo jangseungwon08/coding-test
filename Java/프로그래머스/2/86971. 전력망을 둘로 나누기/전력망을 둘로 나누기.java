e1ccb96230ba659430c47ccdd98a4cde0d2f84e5
jangseungwon <jangseungwon9819@gmail.com>
2026-07-13 00:44:09 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.53 ms, Memory: 76.2 MB -BaekjoonHub

--- Java/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ Java/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[][] wires) {
+     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-        boolean[][] graph = new boolean[n+1][n+1];
-//         연결리스트 만들어서 true면 연결되어있다고 한다.
-        for(int i = 0; i < wires.length; i++){
-            int[] wire = wires[i];
+
+        boolean[][] adjList = new boolean[n+1][n+1];
+        for(int[] wire: wires){
             int u = wire[0];
             int v = wire[1];
-            graph[u][v] = true;
-            graph[v][u] = true;
+            adjList[u][v] = true;
+            adjList[v][u] = true;
         }
-//      한개씩 끊어봄 -> 후 BFS
-        for(int i = 0; i< wires.length; i++){
-            int[] wire = wires[i];
+
+        for(int[] wire: wires){
             int u = wire[0];
             int v = wire[1];
-            graph[u][v] = false;
-            graph[v][u] = false;
-            int count = bfs(n, u, graph);
-            int diff = Math.abs(count - (n-count));
-            answer = Math.min(answer, diff);
-            graph[u][v] = true;
-            graph[v][u] = true;
+            adjList[u][v] = false;
+            adjList[v][u] = false;
+            boolean[] isVisited = new boolean[n+1];
+            isVisited[u] =  true;
+            int count = dfs(n, u, adjList, isVisited);
+            int dif =  Math.abs(count - (n-count));
+            answer = Math.min(dif, answer);
+            adjList[u][v] = true;
+            adjList[v][u] = true;
         }
-        
         return answer;
     }
-    static int bfs(int n, int start, boolean[][] graph){
-        Queue<Integer> q = new ArrayDeque<>();
-        boolean[] isVisited = new boolean[n+1];
+    public int dfs(int n, int start, boolean[][]graph, boolean[] isVisited){
         int count = 1;
-        
-        q.offer(start);
-        isVisited[start] = true;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            for(int i = 1; i<= n; i++){
-//                 해당 노드와 연결되어있고 방문하지 않으면 연결된 것으로 판점
-                if(graph[curr][i] && !isVisited[i]){
-                    q.offer(i);
-                    isVisited[i] = true;
-                    count++;
-                }
+        for(int i = 1; i<= n; i++){
+            if(graph[start][i] && !isVisited[i]){
+                isVisited[i] = true;
+                count += dfs(n, i, graph, isVisited);
             }
         }
         return count;
     }
-    
 }
